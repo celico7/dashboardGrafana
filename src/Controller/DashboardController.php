451891a63f9982004ba2295d827925ddc21cd09a
HOFFMANN CELIA <celia.hoffmann@etu.unistra.fr>
2026-01-28 14:43:55 +0100

    config grafana data

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,39 +8,35 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractController
 {
+    #[Route('/', name: 'app_home')]
     #[Route('/dashboard', name: 'app_dashboard')]
     public function redirectByRole(): Response
     {
-        if ($this->isGranted('ROLE_PROFESSEUR')) {
-            return $this->redirectToRoute('app_prof_dashboard');
-        }
-
-        if ($this->isGranted('ROLE_ETUDIANT')) {
-            return $this->redirectToRoute('app_eleve_dashboard');
-        }
-
-        if ($this->isGranted('ROLE_GESTIONNAIRE')) {
-            return $this->redirectToRoute('app_admin_dashboard');
-        }
-
-        return $this->redirectToRoute('app_login');
+        // Redirection par défaut vers le dashboard élève (pas de sécurité)
+        return $this->redirectToRoute('app_eleve_dashboard');
     }
 
     #[Route('/prof', name: 'app_prof_dashboard')]
     public function prof(): Response
     {
-        return $this->render('dashboard/prof.html.twig');
+        return $this->render('dashboard/prof.html.twig', [
+            'grafana_url' => $_ENV['GRAFANA_URI'] ?? 'http://localhost:3000'
+        ]);
     }
 
     #[Route('/eleve', name: 'app_eleve_dashboard')]
     public function eleve(): Response
     {
-        return $this->render('dashboard/eleve.html.twig');
+        return $this->render('dashboard/eleve.html.twig', [
+            'grafana_url' => $_ENV['GRAFANA_URI'] ?? 'http://localhost:3000'
+        ]);
     }
 
     #[Route('/admin', name: 'app_admin_dashboard')]
     public function admin(): Response
     {
-        return $this->render('dashboard/admin.html.twig');
+        return $this->render('dashboard/admin.html.twig', [
+            'grafana_url' => $_ENV['GRAFANA_URI'] ?? 'http://localhost:3000'
+        ]);
     }
 }
