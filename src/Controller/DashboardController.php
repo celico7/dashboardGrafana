afe544f02416bf5c125f6c27d05689460d941f5a
HOFFMANN CELIA <celia.hoffmann@etu.unistra.fr>
2026-01-29 10:01:24 +0100

    Améliorer la redirection et l'interface utilisateur pour l'authentification

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -9,11 +9,27 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->redirectToRoute('app_login');
+    }
+
     #[Route('/dashboard', name: 'app_dashboard')]
     public function redirectByRole(): Response
     {
-        // Redirection par défaut vers le dashboard élève (pas de sécurité)
-        return $this->redirectToRoute('app_eleve_dashboard');
+        if ($this->isGranted('ROLE_PROFESSEUR')) {
+            return $this->redirectToRoute('app_prof_dashboard');
+        }
+
+        if ($this->isGranted('ROLE_ETUDIANT')) {
+            return $this->redirectToRoute('app_eleve_dashboard');
+        }
+
+        if ($this->isGranted('ROLE_GESTIONNAIRE')) {
+            return $this->redirectToRoute('app_admin_dashboard');
+        }
+
+        return $this->redirectToRoute('app_login');
     }
 
     #[Route('/prof', name: 'app_prof_dashboard')]
